d342e2e7f9f84d86d6d9b7b3a363c35fba9cf9f2
zxcEmpiowervwewqveqw <anonmauidjcwiq@mail.ru>
2024-07-16 01:00:24 +0700

    clear

--- neko_socket/utils/data_zipper/__init__.py
+++ neko_socket/utils/data_zipper/__init__.py
@@ -17,12 +17,4 @@ class DataZipper:
                 result +=  DataZipper.data_split + el.to_bytes(int_bytes, "big")
             else:
                 raise TypeError(f"Can not read {type(el)} type!")
-        # 12 cuz prefix = 8 + (symb count)
-        # print(len(result))
-        # return result
         return zlib.compress(result) if len(result) > 12 else result
-
-
-res = DataZipper.zip(1, "Hello World!", "SecondMessage!", "Heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeewqceqwceqcweqwcecqwcekceqwkceolqw.eqwjekvqwjevkqwjekqwjekqwvjeqwk,vejqwviejqwv,eiuhjv2u3vh12u3vh123vqwhejqwhevqwuhev123h12v8k3h123j1h23k812vkh3v12j3vuh1mllo World", 201, 408)
-# print(len(res))
-# print(res)
